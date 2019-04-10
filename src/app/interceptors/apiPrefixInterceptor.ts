b960c5b59f16b09efdd39338f0d3b871934aa178
rtukalenko <ruslan.tukalenko@gmail.com>
2019-04-11 00:17:25 +0300

    fix check dev mode

--- src/app/interceptors/apiPrefixInterceptor.ts
+++ src/app/interceptors/apiPrefixInterceptor.ts
@@ -10,7 +10,7 @@ export class ApiPrefixInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (!/^(http|https):/i.test(request.url)) {
-            if (isDevMode) {
+            if (isDevMode()) {
                 request = request.clone({url: environment.apiPrefix + request.url});
             } else {
                 request = request.clone({url: environment.host + environment.apiPrefix + request.url});
