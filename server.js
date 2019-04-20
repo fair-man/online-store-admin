fe5af280d15d2505038b27e92fc8787e5d07381e
rtukalenko <ruslan.tukalenko@gmail.com>
2019-04-20 20:16:13 +0300

    change static server

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-//
-// const app = express();
-//
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/'));
-//
-// app.get('/*', function(req,res) {
-//
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
-//
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+//Install express server
+const express = require('express');
+const path = require('path');
 
-var static = require ('node-static');
-var file = new static.Server(__dirname + '/dist');
-require ('http'). createServer (function (request, response) {
-  request.addListener ('end', function () {
-    file.serve (request, response);
-  }). resume ();
-}). listen (process .env.PORT || 3000);
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
