46701c8c58fb4ca1b802fc034e7bedcbc3cedb5e
rtukalenko <ruslan.tukalenko@gmail.com>
2019-06-16 20:34:36 +0300

    add breadcrumbs for providers-view page

--- src/app/pages/providers/providers-view/providers-view.component.ts
+++ src/app/pages/providers/providers-view/providers-view.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {PROVIDERS_PATHS} from '../providers';
+import {Breadcrumb} from '../../../models/breadcrumbs';
+import {ProviderFull} from '../../../models/provider';
+import {BreadcrumbsService} from '../../../shared/breadcrumbs/breadcrumbs.service';
+import {ProvidersService} from '../providers.service';
 
 @Component({
-  selector: 'app-providers-view',
-  templateUrl: './providers-view.component.html',
-  styleUrls: ['./providers-view.component.scss']
+    selector: 'app-providers-view',
+    templateUrl: './providers-view.component.html',
+    styleUrls: ['./providers-view.component.scss']
 })
 export class ProvidersViewComponent implements OnInit {
+    providerData: ProviderFull;
+    breadcrumbs: Breadcrumb[] = [{text: 'Поставщики', url: PROVIDERS_PATHS.providersList}];
 
-  constructor() { }
+    constructor(private breadcrumbsService: BreadcrumbsService,
+                private router: Router,
+                private route: ActivatedRoute,
+                private providersService: ProvidersService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.route.params.subscribe((routeParams) => {
+            this.providerId = routeParams.id;
+
+            this.getProviderData(this.providerId);
+        });
+    }
+
+    getProviderData(providerId) {
+        this.providersService.getProvider(providerId)
+            .subscribe((response) => {
+                this.providerData = response['data'];
+                this.breadcrumbs.push({text: this.providerData.provider_data.name, url: null});
+                this.breadcrumbsService.updateBreadcrumbs(this.breadcrumbs);
+            });
+    }
 
 }
